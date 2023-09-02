da3a1dea877157147733ba5f246610f90cf63d93
bishoe01 <bishoe01@gmail.com>
2023-09-02 21:55:04 +0900

    현재 LINK에 따른 헤더 버튼 활성화

--- dd/src/components/Header.tsx
+++ dd/src/components/Header.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { dur200 } from '../static/stylesVariable';
-import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation, NavigateFunction } from 'react-router-dom';
 
 function Header() {
   const navigate: NavigateFunction = useNavigate();
+  const location = useLocation();
   const tabs: { name: string; link: string }[] = [
     { name: 'HOME', link: '/' },
     { name: '주문대기', link: '/order-list' },
@@ -12,7 +13,16 @@ function Header() {
     { name: '서빙완료', link: '/serving-done' },
     { name: '매출', link: '/sales' },
   ];
-  const [selectedTab, setSelectedTab] = useState<string>(tabs[0].name);
+
+  const [selectedTab, setSelectedTab] = useState<string>('');
+
+  // On component mount or URL change, update the selected tab
+  useEffect(() => {
+    const matchingTab = tabs.find((tab) => tab.link === location.pathname);
+    if (matchingTab) {
+      setSelectedTab(matchingTab.name);
+    }
+  }, [location.pathname]);
 
   return (
     <div className="flex items-center gap-2 mt-20 mb-[40px]">
