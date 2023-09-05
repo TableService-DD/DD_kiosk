e0574ef55406381b9afb28be67965fc2f51c4375
bishoe01 <bishoe01@gmail.com>
2023-09-05 23:33:12 +0900

    [ADD] Carousel부분 추가

--- dd/src/pages/Sales.tsx
+++ dd/src/pages/Sales.tsx
@@ -1,12 +1,13 @@
+import CarouselComponent from '../components/CarouselComponent';
 import Header from '../components/Header';
 
 function Sales() {
   return (
     <>
       <div className="fixed top-0 left-0 h-screen w-screen bg-black -z-10" />
-      <section className="">
-        <Header />
-      </section>
+      <Header />
+
+      <CarouselComponent />
     </>
   );
 }
